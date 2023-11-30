e3f06351a081c3d34c38f2d3142902852c21d69d
ericarodri <erica.cunha@sptech.school>
2023-11-30 20:36:38 -0300

    atualizações captura

--- API_Captura_Indiv/API_Captura-main/power-tech/src/main/kotlin/Captura.kt
+++ API_Captura_Indiv/API_Captura-main/power-tech/src/main/kotlin/Captura.kt
@@ -3,11 +3,18 @@ object Captura {
     fun pegarJanelas(maquinaEscolhida: Int){
 
         var capturajanela = CapturaJanelas()
-        capturajanela.iniciar()
+        capturajanela.iniciarMysql()
 
         var inserts = capturajanela.captura(maquinaEscolhida)
 
         println("$inserts Registro(s) de atividade inseridos em janelas")
+
+        capturajanela.iniciarSqlServer()
+        var inserts2 = capturajanela.captura(maquinaEscolhida)
+
+        println("$inserts2 Registro(s) de atividade inseridos em janelas")
+
+
 
     }
 
